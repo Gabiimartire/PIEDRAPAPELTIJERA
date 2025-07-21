33660143d0976b27071cc2bcbeaa3a290a3af6c9
Gabiimartire <gabbiimartire3@gmail.com>
2025-07-21 15:32:46 -0300

    Mejor estructura

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import time
 import random
 from matcheo import matcheo
-print("¡Bienvenido al juego de Piedra, Papel, Tijera, Lagarto o Spock!")
-time.sleep(1)
 opciones = {
     'piedra':{'tijera': "Piedra aplasta a Tijeras", 'lagarto': 'Piedra aplasta a Lagarto'},
     'papel': {'piedra': "Papel cubre a Piedra", 'spock': "Papel refuta a Spock"},
@@ -10,15 +8,33 @@ opciones = {
     'spock': {'tijera': "Spock destroza a Tijeras", 'piedra': "Spock vaporiza a Piedra"},
     'lagarto': {'papel': "Lagarto come a Papel", 'spock': "Lagarto envenena a Spock"}
 }
-print("Opciones disponibles:") 
-for i, opcion in enumerate(opciones):
-    print(f"{i + 1}. {opcion}")
-print("A la cuenta de tres, elige tu opción:")
+print("¡Bienvenido al juego de Piedra, Papel, Tijera, Lagarto o Spock!")
+time.sleep(1)
+jugador = input("¿Cuál es tu nombre? ")
+print(f"Hola, {jugador.capitalize()}! Vamos a jugar.")
+time.sleep(1)
+print("¿Sabes las reglas?")
+reglas = input("Escribe 'sí' o 'no': ").lower()
+if reglas == 'no':
+    print("Las reglas son las siguientes:")
+    time.sleep(1)
+    print("Juegas tu contra la CPU y cada uno elige una de las siguientes opciones:")   
+    print("1. 'Piedra' que aplasta a Tijeras y Lagarto.")
+    print("2. 'Papel' cubre a Piedra y refuta a Spock.")
+    print("3. 'Tijeras' cortan a Papel y decapitan a Lagarto.")
+    print("4. 'Spock' destroza a Tijeras y vaporiza a Piedra.")
+    print("5. 'Lagarto' come a Papel y envenena a Spock.")
+else:
+    print("¡Genial! Entonces ya sabes cómo jugar.")
+time.sleep(1)    
+input("Presiona Enter para continuar...")
+print("A la cuenta de tres, elige escribe tuu opción:")
 for i in range(3, 0, -1):
     print(i)
     time.sleep(1)
-jugador = input("Elige tu opción: ").lower()
+playerOpcion = input("Elige tu opción: ").lower()
 cpu = random.choice(list(opciones.keys()))
-if(jugador in opciones):
-    matcheo(jugador, cpu, opciones)
+if(playerOpcion in opciones):
+    matcheo(playerOpcion
+, cpu, opciones, jugador)
 else: print("Opción no válida. Por favor, elige una opción válida.")

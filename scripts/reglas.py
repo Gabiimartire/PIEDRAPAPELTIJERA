9ce4043dcd7d7ac1abfc2b6c4fe12c7ed38a14b9
Gabiimartire <gabbiimartire3@gmail.com>
2025-07-22 15:45:57 -0300

    agregando interfaz grafica tkinter

--- scripts/reglas.py
+++ scripts/reglas.py
@@ -1,10 +1,9 @@
-import time
 def mostrar_reglas():    
-    print("Las reglas son las siguientes:")
-    time.sleep(1)
-    print("Juegas tu contra la CPU y cada uno elige una de las siguientes opciones:")   
-    print("1. 'Piedra' que aplasta a Tijeras y Lagarto.")
-    print("2. 'Papel' cubre a Piedra y refuta a Spock.")
-    print("3. 'Tijeras' cortan a Papel y decapitan a Lagarto.")
-    print("4. 'Spock' destroza a Tijeras y vaporiza a Piedra.")
-    print("5. 'Lagarto' come a Papel y envenena a Spock.")
+    reglas = "Las reglas son las siguientes:" 
+    reglas += "\nJuegas tu contra la CPU y cada uno elige una de las siguientes opciones:"
+    reglas += "\n1. 'Piedra' que aplasta a Tijeras y Lagarto."
+    reglas += "\n2. 'Papel' cubre a Piedra y refuta a Spock."
+    reglas += "\n3. 'Tijeras' cortan a Papel y decapitan a Lagarto."
+    reglas += "\n4. 'Spock' destroza a Tijeras y vaporiza a Piedra."
+    reglas += "\n5. 'Lagarto' come a Papel y envenena a Spock."
+    return reglas
